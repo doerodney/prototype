f9ab16feed75d221dc06adf211901d5c99689e09
doerodney <doerodney@gmail.com>
2022-09-02 16:32:11 -0400

    Unit test implementation, continued.

--- c/matrix/test/unit_tests.c
+++ c/matrix/test/unit_tests.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 void TestIsSingular();
 void TestIsNotSingular();
+
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2 && !strcmp(argv[1], "1") ) {
+        TestIsSingular();
+    }
+    
+    if (argc < 2 && !strcmp(argv[1], "2") ) {
+        TestIsSingular();
+    }
+}
